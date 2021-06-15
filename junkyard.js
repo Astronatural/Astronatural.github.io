55548591dd64d02d466962311b1a9142301cb671
JDK <jdkranzler@yahoo.com>
2021-06-15 16:33:36 -0500

    css scrounger

---
+++ junkyard.js
@@ -0,0 +1,10 @@
+color list:
+
+let salmon = #fe9263;
+let text purple = #9564a1
+let hover text purple = #6f4c78;
+
+template colors:
+
+let template teal = #64a19d;
+let template hover teal = #467370;
